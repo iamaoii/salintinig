c87e225f3c96898e9321ecda4550537cf6aee98c
AJ <sadullo28@gmail.com>
2026-07-18 21:45:29 +0800

    style: set activities card background color to white

--- mobile/lib/pages/student/activities/activities_page.dart
+++ mobile/lib/pages/student/activities/activities_page.dart
@@ -329,7 +329,7 @@ class _ActivitiesPageState extends State<ActivitiesPage> {
                                       iconSvg: PhIcons.userSoundBold,
                                       iconColor: const Color(0xFF2563EB),
                                       iconBgColor: const Color(0xFFDBEAFE),
-                                      cardBgColor: const Color(0xFFEFF6FF),
+                                      cardBgColor: Colors.white,
                                       borderShadowColor: const Color(0xFFBFDBFE),
                                       onTap: () {
                                         Navigator.push(
@@ -349,7 +349,7 @@ class _ActivitiesPageState extends State<ActivitiesPage> {
                                       iconSvg: PhIcons.equalsBold,
                                       iconColor: const Color(0xFFD97706),
                                       iconBgColor: const Color(0xFFFEF3C7),
-                                      cardBgColor: const Color(0xFFFFFBEB),
+                                      cardBgColor: Colors.white,
                                       borderShadowColor: const Color(0xFFFDE68A),
                                       onTap: () {
                                         Navigator.push(
@@ -369,7 +369,7 @@ class _ActivitiesPageState extends State<ActivitiesPage> {
                                       iconSvg: PhIcons.hammerBold,
                                       iconColor: const Color(0xFF059669),
                                       iconBgColor: const Color(0xFFD1FAE5),
-                                      cardBgColor: const Color(0xFFECFDF5),
+                                      cardBgColor: Colors.white,
                                       borderShadowColor: const Color(0xFFA7F3D0),
                                       onTap: () {
                                         Navigator.push(
